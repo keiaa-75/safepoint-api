bcbf86184215e8a0536d9ab13ec5be2bcb5e34ed
Keiaa <confinedrose@duck.com>
2026-02-24 12:38:05 +0800

    feat: re-establish student-related classes

--- src/main/java/com/keiaa/safepoint_api/SafepointApiApplication.java
+++ src/main/java/com/keiaa/safepoint_api/SafepointApiApplication.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright © 2026, Jill Abarte <keiaa.07.05.00@gmail.com>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.keiaa.safepoint_api;
 
 import org.springframework.boot.SpringApplication;
