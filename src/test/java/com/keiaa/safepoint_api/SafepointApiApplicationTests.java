7f3c40c8e635bba47c68662168b91f8b5fadc22b
Keiaa <confinedrose@duck.com>
2026-02-24 09:33:52 +0800

    chore: initialized Spring project

---
+++ src/test/java/com/keiaa/safepoint_api/SafepointApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keiaa.safepoint_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafepointApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
